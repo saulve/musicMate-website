8ac19a5305808ce65ff1c79a0a59c562aa248d02
saulvenskutonis <saulvenskutonis@gmail.com>
2018-08-30 01:47:11 +0100

    fix a typo

--- strings.js
+++ strings.js
@@ -1,6 +1,6 @@
 export const description =
   "musicMate is a browser extension, which enables effortless discovery of performances simply by listening to your favorite artists on Spotify";
 export const intro =
-  "Everyone can Google an artist to check for nearby perfomances, but in reality how many of us have time to do that? There should be a better (and lazier) solution. One that would require <b>zero</b> effort.";
+  "Everyone can Google an artist to check for nearby performances, but in reality how many of us have time to do that? There should be a better (and lazier) solution. One that would require <b>zero</b> effort.";
 export const presentation =
   "MusicMate removes the hassle of concert-hunting and lets you relax and enjoy the music instead. Simply log in with your Spotify account, enter your location, and the distance you're willing to travel for a concert and you're good to go!<br/><br/>Whenever the app detects your currently listened artist performing nearby you'll get a notification. It works even if you're listening on your smartphone! No more missed chances - <em>let the concerts find you</em>.";
